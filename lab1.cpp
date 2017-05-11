21c1ad615af0f7645c17fdb1cae629083bc7e681
Hayden Yu <hayden@Hayden-Mac.local>
2017-05-11 11:14:35 -0400

    power function completed

--- lab1.cpp
+++ lab1.cpp
@@ -8,7 +8,11 @@ unsigned int factorial(unsigned int n){
     return res == 0 ? (unsigned)1 : res;
 }
 double power (double base, unsigned int n){
-    
+    double res = base;
+    for(unsigned i = 1; i < n; ++i){
+        res *= base;
+    } 
+    return res;
 }
 unsigned int fibonacci (unsigned int n){
     
