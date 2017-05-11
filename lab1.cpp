6d7c6165a3ac1a41d31dd091ec79384287a028a8
Hayden Yu <hayden@Hayden-Mac.local>
2017-05-11 11:08:15 -0400

    factorial function completed

--- lab1.cpp
+++ lab1.cpp
@@ -1,7 +1,11 @@
 /*starter file for lab1*/
 
 unsigned int factorial(unsigned int n){
-    
+    unsigned res = n;
+    while(n > 1) {
+        res *= --n;
+    }
+    return res == 0 ? (unsigned)1 : res;
 }
 double power (double base, unsigned int n){
     
